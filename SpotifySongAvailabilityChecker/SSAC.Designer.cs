6c4f0193e9018b0a1b5c59adcc1d208776f910e4
Hashim Qureshi <42122270+CasualHonest@users.noreply.github.com>
2021-05-18 11:07:30 -0400

    Add files for WIP program

---
+++ SpotifySongAvailabilityChecker/SSAC.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace SpotifySongAvailabilityChecker
+{
+    partial class SSAC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAvailability = new System.Windows.Forms.ListBox();
+            this.txtTrackID = new System.Windows.Forms.TextBox();
+            this.lblTrackID = new System.Windows.Forms.Label();
+            this.txtAlbumID = new System.Windows.Forms.TextBox();
+            this.lblAlbumID = new System.Windows.Forms.Label();
+            this.btnCheckAvailability = new System.Windows.Forms.Button();
+            this.chkIsAlbum = new System.Windows.Forms.CheckBox();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblContentTitle = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.lblToken = new System.Windows.Forms.Label();
+            this.btnToken = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAvailability
+            // 
+            this.lstAvailability.FormattingEnabled = true;
+            this.lstAvailability.Location = new System.Drawing.Point(331, 18);
+            this.lstAvailability.Name = "lstAvailability";
+            this.lstAvailability.Size = new System.Drawing.Size(312, 212);
+            this.lstAvailability.TabIndex = 2;
+            // 
+            // txtTrackID
+            // 
+            this.txtTrackID.Location = new System.Drawing.Point(70, 22);
+            this.txtTrackID.Name = "txtTrackID";
+            this.txtTrackID.Size = new System.Drawing.Size(255, 20);
+            this.txtTrackID.TabIndex = 4;
+            // 
+            // lblTrackID
+            // 
+            this.lblTrackID.AutoSize = true;
+            this.lblTrackID.Location = new System.Drawing.Point(12, 25);
+            this.lblTrackID.Name = "lblTrackID";
+            this.lblTrackID.Size = new System.Drawing.Size(52, 13);
+            this.lblTrackID.TabIndex = 3;
+            this.lblTrackID.Text = "Track ID:";
+            // 
+            // txtAlbumID
+            // 
+            this.txtAlbumID.Location = new System.Drawing.Point(70, 50);
+            this.txtAlbumID.Name = "txtAlbumID";
+            this.txtAlbumID.Size = new System.Drawing.Size(255, 20);
+            this.txtAlbumID.TabIndex = 6;
+            // 
+            // lblAlbumID
+            // 
+            this.lblAlbumID.AutoSize = true;
+            this.lblAlbumID.Location = new System.Drawing.Point(12, 53);
+            this.lblAlbumID.Name = "lblAlbumID";
+            this.lblAlbumID.Size = new System.Drawing.Size(53, 13);
+            this.lblAlbumID.TabIndex = 5;
+            this.lblAlbumID.Text = "Album ID:";
+            // 
+            // btnCheckAvailability
+            // 
+            this.btnCheckAvailability.Location = new System.Drawing.Point(55, 208);
+            this.btnCheckAvailability.Name = "btnCheckAvailability";
+            this.btnCheckAvailability.Size = new System.Drawing.Size(112, 23);
+            this.btnCheckAvailability.TabIndex = 7;
+            this.btnCheckAvailability.Text = "Check Availability";
+            this.btnCheckAvailability.UseVisualStyleBackColor = true;
+            this.btnCheckAvailability.Click += new System.EventHandler(this.btnCheckAvailability_Click);
+            // 
+            // chkIsAlbum
+            // 
+            this.chkIsAlbum.AutoSize = true;
+            this.chkIsAlbum.Location = new System.Drawing.Point(15, 112);
+            this.chkIsAlbum.Name = "chkIsAlbum";
+            this.chkIsAlbum.Size = new System.Drawing.Size(72, 17);
+            this.chkIsAlbum.TabIndex = 8;
+            this.chkIsAlbum.Text = "Is Album?";
+            this.chkIsAlbum.UseVisualStyleBackColor = true;
+            this.chkIsAlbum.CheckedChanged += new System.EventHandler(this.chkIsAlbum_CheckedChanged);
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(51, 170);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(44, 13);
+            this.lblAuthor.TabIndex = 11;
+            this.lblAuthor.Text = "Author: ";
+            // 
+            // lblContentTitle
+            // 
+            this.lblContentTitle.AutoSize = true;
+            this.lblContentTitle.Location = new System.Drawing.Point(51, 144);
+            this.lblContentTitle.Name = "lblContentTitle";
+            this.lblContentTitle.Size = new System.Drawing.Size(33, 13);
+            this.lblContentTitle.TabIndex = 10;
+            this.lblContentTitle.Text = "Title: ";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(70, 78);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(255, 20);
+            this.txtToken.TabIndex = 13;
+            // 
+            // lblToken
+            // 
+            this.lblToken.AutoSize = true;
+            this.lblToken.Location = new System.Drawing.Point(12, 81);
+            this.lblToken.Name = "lblToken";
+            this.lblToken.Size = new System.Drawing.Size(41, 13);
+            this.lblToken.TabIndex = 12;
+            this.lblToken.Text = "Token:";
+            // 
+            // btnToken
+            // 
+            this.btnToken.Location = new System.Drawing.Point(173, 208);
+            this.btnToken.Name = "btnToken";
+            this.btnToken.Size = new System.Drawing.Size(112, 23);
+            this.btnToken.TabIndex = 17;
+            this.btnToken.Text = "Get Token";
+            this.btnToken.UseVisualStyleBackColor = true;
+            this.btnToken.Click += new System.EventHandler(this.btnToken_Click);
+            // 
+            // SSAC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 243);
+            this.Controls.Add(this.btnToken);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.lblToken);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblContentTitle);
+            this.Controls.Add(this.chkIsAlbum);
+            this.Controls.Add(this.btnCheckAvailability);
+            this.Controls.Add(this.txtAlbumID);
+            this.Controls.Add(this.lblAlbumID);
+            this.Controls.Add(this.txtTrackID);
+            this.Controls.Add(this.lblTrackID);
+            this.Controls.Add(this.lstAvailability);
+            this.Name = "SSAC";
+            this.Text = "Spotify Song Availability Checker";
+            this.Load += new System.EventHandler(this.SSAC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstAvailability;
+        private System.Windows.Forms.TextBox txtTrackID;
+        private System.Windows.Forms.Label lblTrackID;
+        private System.Windows.Forms.TextBox txtAlbumID;
+        private System.Windows.Forms.Label lblAlbumID;
+        private System.Windows.Forms.Button btnCheckAvailability;
+        private System.Windows.Forms.CheckBox chkIsAlbum;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblContentTitle;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label lblToken;
+        private System.Windows.Forms.Button btnToken;
+    }
+}
+
---
+++ SpotifySongAvailabilityChecker/SSAC.cs
@@ -0,0 +1,144 @@
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SpotifySongAvailabilityChecker
+{
+    public partial class SSAC : Form
+    {
+        static SpotifyClient client;
+        RegionInfo albumInfo;
+        RegionInfo trackInfo;
+
+        public SSAC()
+        {
+            InitializeComponent();
+        }
+
+        private void SSAC_Load(object sender, EventArgs e)
+        {
+            txtAlbumID.Enabled = false;
+        }
+
+        private void chkIsAlbum_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkIsAlbum.Checked)
+            {
+                txtAlbumID.Enabled = true;
+                txtTrackID.Enabled = false;
+            }
+            else
+            {
+                txtAlbumID.Enabled = false;
+                txtTrackID.Enabled = true;
+            }
+        }
+
+        private void btnCheckAvailability_Click(object sender, EventArgs e)
+        {
+            /*if (client == null)
+            {
+                MessageBox.Show("You need to get an access token by \"Get Token\" to continue", "No token provided", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }*/
+
+            try
+            {
+                client = new SpotifyClient(txtToken.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (chkIsAlbum.Checked)
+            {
+                if (string.IsNullOrWhiteSpace(txtAlbumID.Text))
+                {
+                    MessageBox.Show("Please enter in an album ID to continue", "Missing input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                FullAlbum album = client.Albums.Get(txtAlbumID.Text).Result;
+                lblContentTitle.Text += album.Name;
+                lblAuthor.Text += album.Artists[0].Name;
+                lstAvailability.Items.Clear();
+                foreach (string s in album.AvailableMarkets)
+                {
+                    try
+                    {
+                        albumInfo = new RegionInfo(s.ToLowerInvariant());
+                        lstAvailability.Items.Add(string.Concat(s, " - ", albumInfo.DisplayName));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(txtTrackID.Text))
+                {
+                    MessageBox.Show("Please enter in a track ID to continue", "Missing input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                FullTrack track = client.Tracks.Get(txtTrackID.Text).Result;
+                lblContentTitle.Text += track.Name;
+                lblAuthor.Text += track.Artists[0].Name;
+                lstAvailability.Items.Clear();
+                foreach (string s in track.AvailableMarkets)
+                {
+                    try
+                    {
+                        trackInfo = new RegionInfo(s.ToLowerInvariant());
+                        lstAvailability.Items.Add(string.Concat(s, " - ", trackInfo.DisplayName));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnToken_Click(object sender, EventArgs e)
+        {
+            LoginRequest request = new LoginRequest(new Uri("https://webpages.uncc.edu/hquresh1/SpotifyRedirect/callback/"), "4f8281c491f244d0a4b2058dbb4587a6", LoginRequest.ResponseType.Token);
+            Uri requestUri = request.ToUri();
+            BrowserUtil.Open(requestUri);
+        }
+
+        /**
+         * Credit to Brock Allen for finding this hack how to make URL's load cross-platform
+         * URL here: https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+         */
+        private void runUrlLoad(string Url)
+        {
+            try
+            {
+                Process.Start(Url);
+            }
+            catch
+            {
+                // This area checks the platform the program is running on and then starts a new process with the URL and with different commands depending on the system
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Url = Url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {Url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    Process.Start("xdg-open", Url);
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    Process.Start("open", Url);
+                else
+                    throw;
+            }
+        }
+    }
+}
